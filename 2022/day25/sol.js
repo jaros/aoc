186b602d2e184dc4b2e78556fc7d5f396f7aeb0b
xp <xp@xp-u80a>
2022-12-25 12:42:37 +0200

    calc to elf base

--- 2022/day25/sol.js
+++ 2022/day25/sol.js
@@ -23,20 +23,66 @@ let toDecimal = (snafu) => {
   return sum;
 }
 
+// 2=-1=0
+// 5 -> 10
 let toSnafu = (decimalNum) => {
   let sum = '';
-  let decStr = (decimalNum+'').split('');
+//  let decStr = (decimalNum+'').split('');
+  let base = 5;
+  let k=1;
+  let fullPart = 5;
+  while (decimalNum >= fullPart) {
+    fullPart = fullPart * base; 
+    k++;
+    //console.log('full part', fullPart, 'k', k);
+  } 
 
-  let shift = -2;
+  k--;
+  fullPart /= base;
 
-
-  for (let i = decStr.length-1; i >= 0; i--) {
-    let val = decStr[i];
-
+  //console.log('==> ',k, fullPart)
+  //let prevDiff = 0;
+  while (k > 0) {
+   let t = Math.floor(decimalNum / fullPart);
+   //t += prevDiff;
+   if (t == 3) {
+    //t = 1;
+    preDiff = 2;
+   } else if (t == 4) {
+    //t =1;
+   }
+   sum += t;
+   decimalNum -= t * fullPart;
+   fullPart /= base;
+   k--;
   }
+  sum += decimalNum;
 
-  console.log(decStr)
-  return decStr;
+  let passOver = 0;
+  let parts = sum.split('');
+  console.log(sum)
+  for (let i=parts.length - 1; i >= 0; i--) {
+    let num = Number(parts[i]);
+    num += passOver;
+    passOver = 0;
+    parts[i] = num; 
+    if (num == 3) {
+      parts[i] = '=';
+      passOver = 1;
+    } else if (num == 4) {
+      parts[i] = '-';
+      passOver = 1;
+    }
+    else if (num > 4) {
+      passOver = num % base;
+    }
+    
+  }
+  if (passOver > 0) {
+    parts.unshift(passOver);
+  }
+	
+  return parts.join(''); 
 }
 
 let calculate = (inputs) => {
@@ -56,6 +102,13 @@ let calculate = (inputs) => {
 
 const inputs = readInput();
 let start = new Date().getTime();
-calculate(inputs);
+// calculate(inputs);
+// toSnafu(125)
+console.log('7 -> 12 ', toSnafu(7));
+console.log('5 -> 10 ', toSnafu(5));
+console.log('10 -> 20 ', toSnafu(10));
+console.log('3 -> 1= ', toSnafu(3));
+console.log('4 -> 1- ', toSnafu(4));
+console.log('8 -> 2= ', toSnafu(8))
 console.log(`spent time: ${new Date().getTime() - start} ms`);
 
