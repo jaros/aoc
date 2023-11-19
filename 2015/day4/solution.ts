35ea67fbf12d5061c895f8dc8c94293eecd3c90d
jaros <judin.jaroslav@gmail.com>
2023-11-19 22:20:21 +0200

    add day4 2015

--- 2015/day4/solution.ts
+++ 2015/day4/solution.ts
@@ -1,12 +1,29 @@
 
 import { Solution, readInput, title, withTime } from "../../common/types";
 
+const {
+  createHash
+} = await import('node:crypto');
+
+const findNumber = (prefix: string, leadingZerosInHash: string) => {
+  let i = 0;
+  let digest = "";
+  while (!digest.startsWith(leadingZerosInHash)) {
+    i++;
+    const hash = createHash('md5');
+    hash.update(prefix + i);
+    digest = hash.digest('hex')
+  }
+  return i;
+}
+
 const part1 = (data: string) => {
-  return 0;
+  return findNumber(data, "00000");
 };
 
+// maybe it's not so efficient but on ARM machine (M2) in 2023 it takes only 4624 ms
 const part2 = (data: string) => {
-  return 0;
+  return findNumber(data, "000000");
 };
 
 export const solve: Solution = (source) => {
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
-import {solve} from "./2015/day3/solution"
+import {solve} from "./2015/day4/solution"
 import { Source } from "./common/types";
 
 const source = process.argv[2] == 'test' ? Source.TEST : Source.INPUT;
 
 solve(source);
+// e80b5017098950fc58aad83c8c14978e
+// 000001dbbfa3a5c83a2d506429c7b00e
