6c5504fbfca65f0286fd5924f0b72dd7eb498a17
jaros <judin.jaroslav@gmail.com>
2023-12-05 23:16:07 +0200

    pass day as param

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
-import {solve} from "./2023/day6/solution"
 import { Source } from "./common/types";
 
-const source = process.argv[2] == 'test' ? Source.TEST : Source.INPUT;
+const day = parseInt(process.argv[2]!, 10);
+if (Number.isNaN(day)) {
+    throw new Error('Invalid error')
+}
+
+const module = `./2023/day${day}/solution`;
+const { solve } = await import(module)
+
+const source = process.argv[3] == 'test' ? Source.TEST : Source.INPUT;
 
 solve(source);
