761f6b7f71403d63e1564b2711659d7df4df50ed
jaros <judin.jaroslav@gmail.com>
2023-12-09 22:12:00 +0200

    prepare 2023 day 10

---
+++ 2023/day10/solution.ts
@@ -0,0 +1,23 @@
+
+import { Solution, readInput, title, withTime } from "../../common/types";
+import { sum } from "../utils";
+
+const parseInput = (data: string) : number[][] => {
+  return data.split("\n").map(line => line.split(" ").map(Number));
+}
+
+const part1 = (data: string) => {
+  return 0;
+}
+
+const part2 = (data: string) => {
+  return 0;
+}
+
+
+export const solve: Solution = (source) => {
+  title("Day 10: xxx");
+  const data = readInput(source, import.meta.dir)
+  withTime(part1)(data);
+  withTime(part2)(data);
+};
