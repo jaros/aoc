acd74d4f4ed311c2e64c3a481d24d22860500591
jaros <judin.jaroslav@gmail.com>
2023-12-10 18:22:27 +0200

    2023 day10 part2

--- 2023/day10/solution.ts
+++ 2023/day10/solution.ts
@@ -96,7 +96,6 @@ let next = (curr: Pos, prev: Pos, m: string[][]): Pos => {
   for (let {adj, to} of adjacents) {
     let adjPos = {i: curr.i + adj.i, j: curr.j +adj.j};
     let adjVal = m[adjPos.i]?.[adjPos.j];
-    // console.log("checking", adjVal, "to", to)
     if (adjVal && arePipesMatch(to, currVal, adjVal) && !isSamePos(adjPos, prev)) {
       return adjPos;
     }
@@ -105,70 +104,150 @@ let next = (curr: Pos, prev: Pos, m: string[][]): Pos => {
   throw new Error('no adjacent pipe found')
 }
 
+let flipDir = (dir: Dir): Dir => {
+  switch(dir) {
+    case "E": return "W";
+    case "W": return "E";
+    case "S": return "N";
+    case "N": return "S";
+  }
+}
+
 let findStartNext = (start: Pos, m: string[][]) => {
   let twoAdjacents: Pos[] = [];
 
-  // check from south-to-x => i direction is inversed to y 
-  let adjPos = {i: start.i + 1, j: start.j};
-  let adjVal = m[adjPos.i][adjPos.j];
-  if (adjVal == '|' || adjVal == 'L' || adjVal == 'J') {
-    twoAdjacents.push({i: adjPos.i, j: adjPos.j});
+  for (let {to, adj} of adjacents) {
+    let adjPos = {i: start.i + adj.i, j: start.j + adj.j};
+    let adjVal = m[adjPos.i][adjPos.j];
+    if (toDirMatches[flipDir(to)]?.includes(adjVal)) {
+      twoAdjacents.push({i: adjPos.i, j: adjPos.j});
+    }
   }
-
-
-  // check from west-to-x
-  adjPos = {i: start.i, j: start.j + 1};
-  adjVal = m[adjPos.i][adjPos.j];
-  if (adjVal == '-' || adjVal == 'J' || adjVal == '7') {
-    twoAdjacents.push({i: adjPos.i, j: adjPos.j});
-  }
-
-
-  // check from north-to-x
-  adjPos = {i: start.i - 1, j: start.j};
-  adjVal = m[adjPos.i][adjPos.j];
-  if (adjVal == '|' || adjVal == '7' || adjVal == 'F') {
-    twoAdjacents.push({i: adjPos.i, j: adjPos.j});
-  }
-
-  // check from east-to-x
-  adjPos = {i: start.i, j: start.j - 1};
-  adjVal = m[adjPos.i][adjPos.j];
-  if (adjVal == '-' || adjVal == 'L' || adjVal == 'F') {
-    twoAdjacents.push({i: adjPos.i, j: adjPos.j});
-  }
-
-  console.log("twoAdjacents", twoAdjacents)
-
   if (twoAdjacents.length !== 2) {
     throw new Error('NOT TWO adjacents at start');
   }
-  return twoAdjacents[1];
+  return twoAdjacents[0];
+}
+
+
+export const findLoop = (data: string): Pos[]  => {
+  let maze: string[][] = parseInput(data)
+  let start: Pos = findStartPos(maze);
+
+  let nextToStart = findStartNext(start, maze);
+  let steps = [];
+  let prev: Pos = start;
+  let curr: Pos = nextToStart;
+  do {
+    steps.push(prev);
+    let tmp = curr;
+    curr = next(curr, prev, maze);
+    prev = tmp;
+  } while (!isSamePos(curr, start))
+  steps.push(prev);
+  return steps;
 }
 
 const part1 = (data: string) => {
-  let maze: string[][] = parseInput(data)
-  let start: Pos = findStartPos(maze);
-  console.log("start", start)
+  return Math.floor(findLoop(data).length/2);
+}
 
-  let nextToStart = findStartNext(start, maze);
-  let steps = 1;
-  let prev: Pos = start;
-  let curr: Pos = nextToStart;
-  console.log("nextToStart", maze[curr.i][curr.j], curr)
-  do {
-    let tmp = curr;
-    curr = next(curr, prev, maze);
-    // console.log("next", maze[curr.i][curr.j], curr)
-    prev = tmp;
-    steps++;
-  } while (!isSamePos(curr, start))
+let getAreaMap = (
+  input: string
+): [string[][], Array<[number, number, number, number]>] => {
 
-  return Math.floor(steps/2);
+  const loop = findLoop(input);
+
+  const loopTiles = new Set<string>(loop.map(({i, j}) => `${i}:${j}`));
+  const loopEdges: Array<[number, number, number, number]> = [];
+
+  const lines = parseInput(input);
+
+  const result: string[][] = [];
+
+  for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      const l: string[] = [];
+
+      for (let j = 0; j < line.length; j++) {
+          const char = loopTiles.has(`${i}:${j}`) ? line[j] : '.';
+          l.push(char);
+      }
+
+      result.push(l);
+  }
+
+  let prev = loop[loop.length - 1];
+  for (const next of loop) {
+      loopEdges.push([
+          prev.i + 0.5,
+          prev.j + 0.5,
+          next.i + 0.5,
+          next.j + 0.5
+      ]);
+      prev = next;
+  }
+
+  return [result, loopEdges];
+}
+
+let insideLoopTiles = (
+  x: number,
+  y: number,
+  loopEdges: Array<[number, number, number, number]>
+): number => {
+  let intersections = 0;
+
+  for (const [x1, y1, x2, y2] of loopEdges) {
+      intersections += intersectEdges(x, y, 0, y, x1, y1, x2, y2);
+  }
+
+  return intersections % 2;
+}
+
+let intersectEdges = (
+  p0_x: number,
+  p0_y: number,
+  p1_x: number,
+  p1_y: number,
+  p2_x: number,
+  p2_y: number,
+  p3_x: number,
+  p3_y: number
+): number => {
+  const s1_x = p1_x - p0_x;
+  const s1_y = p1_y - p0_y;
+  const s2_x = p3_x - p2_x;
+  const s2_y = p3_y - p2_y;
+
+  const s =
+      (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) /
+      (-s2_x * s1_y + s1_x * s2_y);
+  const t =
+      (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) /
+      (-s2_x * s1_y + s1_x * s2_y);
+
+  if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
+      return 1;
+  }
+
+  return 0; // No collision
 }
 
 const part2 = (data: string) => {
-  return 0;
+
+  const [matrix, loopEdges] = getAreaMap(data);
+
+  let counter = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === '.') {
+        counter += insideLoopTiles(i, j, loopEdges);
+      }
+    }
+  }
+
+  return counter;
 }
 
 
