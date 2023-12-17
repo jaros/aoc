4cb148b06ee28152a2f1c400ffeb92cac3f1fd2d
jaros <judin.jaroslav@gmail.com>
2023-12-17 12:34:48 +0200

    2023 day17 all

--- 2023/day17/solution.ts
+++ 2023/day17/solution.ts
@@ -1,16 +1,95 @@
 
 import { Solution, readInput, title, withTime } from "../../common/types";
+import { PriorityQueue } from "../utils";
 
 const part1 = (data: string) => {
+  let grid: number[][] = data.split('\n').map(line => line.trim().split('').map(Number))
+
+  const seen = new Set();
+  const pq = new PriorityQueue();
+  pq.push([0, 0, 0, 0, 0, 0]);
+
+  while (!pq.isEmpty()) {
+    const [hl, r, c, dr, dc, n] = pq.pop()!;
+
+    if (r === grid.length - 1 && c === grid[0].length - 1) {
+      return hl;
+    }
+
+    if (seen.has(`${r}-${c}-${dr}-${dc}-${n}`)) {
+      continue;
+    }
+
+    seen.add(`${r}-${c}-${dr}-${dc}-${n}`);
+
+    if (n < 3 && (dr !== 0 || dc !== 0)) {
+      const nr = r + dr;
+      const nc = c + dc;
+      if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length) {
+        pq.push([hl + grid[nr][nc], nr, nc, dr, dc, n + 1]);
+      }
+    }
+
+    const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    for (const [ndr, ndc] of directions) {
+      if ((ndr !== dr || ndc !== dc) && (ndr !== -dr || ndc !== -dc)) {
+        const nr = r + ndr;
+        const nc = c + ndc;
+        if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length) {
+          pq.push([hl + grid[nr][nc], nr, nc, ndr, ndc, 1]);
+        }
+      }
+    }
+  }
   return 0;
 }
 
 const part2 = (data: string) => {
+  let grid: number[][] = data.split('\n').map(line => line.trim().split('').map(Number))
+
+  const seen = new Set();
+  const pq = new PriorityQueue();
+  pq.push([0, 0, 0, 0, 0, 0]);
+
+  while (!pq.isEmpty()) {
+    const [hl, r, c, dr, dc, n] = pq.pop()!;
+
+    if (r === grid.length - 1 && c === grid[0].length - 1 && n >= 4) {
+      return hl;
+    }
+
+    if (seen.has(`${r}-${c}-${dr}-${dc}-${n}`)) {
+      continue;
+    }
+
+    seen.add(`${r}-${c}-${dr}-${dc}-${n}`);
+
+    if (n < 10 && (dr !== 0 || dc !== 0)) {
+      const nr = r + dr;
+      const nc = c + dc;
+      if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length) {
+        pq.push([hl + grid[nr][nc], nr, nc, dr, dc, n + 1]);
+      }
+    }
+
+    if (n >= 4 || (dr == 0 && dc == 0)) {
+      const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+      for (const [ndr, ndc] of directions) {
+        if ((ndr !== dr || ndc !== dc) && (ndr !== -dr || ndc !== -dc)) {
+          const nr = r + ndr;
+          const nc = c + ndc;
+          if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length) {
+            pq.push([hl + grid[nr][nc], nr, nc, ndr, ndc, 1]);
+          }
+        }
+      }
+    }
+  }
   return 0;
 }
 
 export const solve: Solution = (source) => {
-  title("Day 17: XXX");
+  title("Day 17: Clumsy Crucible");
   const data = readInput(source, import.meta.dir)
   withTime(part1)(data);
   withTime(part2)(data);
--- 2023/utils/index.ts
+++ 2023/utils/index.ts
@@ -4,4 +4,78 @@ export const sum = (a: number, b: number) => a + b;
 
 export const multiply = (a: number, b: number) => a * b;
 
-export const count = (line: string, char: string): number => line.split('').filter(c => c == char).length 
+export const count = (line: string, char: string): number => line.split('').filter(c => c == char).length;
+
+export class PriorityQueue {
+    private heap: number[][];
+    constructor() {
+        this.heap = [];
+      }
+    
+      push(item: number[]) {
+        this.heap.push(item);
+        this.heapifyUp();
+      }
+    
+      pop() {
+        if (this.isEmpty()) return null;
+        const root = this.heap[0];
+        const last = this.heap.pop();
+        if (this.heap.length > 0) {
+          this.heap[0] = last!;
+          this.heapifyDown();
+        }
+        return root;
+      }
+    
+      isEmpty() {
+        return this.heap.length === 0;
+      }
+    
+      heapifyUp() {
+        let currentIndex = this.heap.length - 1;
+        while (currentIndex > 0) {
+          const parentIndex = Math.floor((currentIndex - 1) / 2);
+          if (this.heap[currentIndex][0] < this.heap[parentIndex][0]) {
+            [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
+            currentIndex = parentIndex;
+          } else {
+            break;
+          }
+        }
+      }
+    
+      heapifyDown() {
+        let currentIndex = 0;
+        const length = this.heap.length;
+        const currentElement = this.heap[0];
+    
+        while (true) {
+          let leftChildIndex = 2 * currentIndex + 1;
+          let rightChildIndex = 2 * currentIndex + 2;
+          let leftChild, rightChild;
+          let swapIndex = null;
+    
+          if (leftChildIndex < length) {
+            leftChild = this.heap[leftChildIndex];
+            if (leftChild[0] < currentElement[0]) {
+              swapIndex = leftChildIndex;
+            }
+          }
+    
+          if (rightChildIndex < length) {
+            rightChild = this.heap[rightChildIndex];
+            if ((swapIndex === null && rightChild[0] < currentElement[0]) ||
+                (swapIndex !== null && rightChild[0] < leftChild![0])) {
+              swapIndex = rightChildIndex;
+            }
+          }
+    
+          if (swapIndex === null) break;
+    
+          this.heap[currentIndex] = this.heap[swapIndex];
+          this.heap[swapIndex] = currentElement;
+          currentIndex = swapIndex;
+        }
+      }
+  }
