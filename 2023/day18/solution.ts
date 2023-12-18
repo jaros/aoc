11c6ea8fdac347cff612731ac0064f98a04c46bb
Jaroslav Judin <jaroslav.judin@bolt.eu>
2023-12-18 13:49:57 +0200

    update formulas sources

--- 2023/day18/solution.ts
+++ 2023/day18/solution.ts
@@ -47,11 +47,15 @@ const part2 = (data: string) => {
     let [r, c] = points[points.length - 1];
     points.push([r + dx * n, c + dy * n]);
   }
+
+  // https://en.wikipedia.org/wiki/Shoelace_formula
   let sum = 0;
   for (let i = 0; i < points.length - 1; i++) {
     sum += points[i][0] * (points[(i-1 + points.length) % points.length][1] - points[(i+1) % points.length][1]) 
   }
   let A = Math.abs(sum) / 2;
+
+  // https://en.wikipedia.org/wiki/Pick%27s_theorem
   let i = A - b/2 + 1;
 
   return i + b;
