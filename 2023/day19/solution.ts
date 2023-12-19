c88d03ceaa25e3797d5287aa55e603a210cc0ee2
jaros <judin.jaroslav@gmail.com>
2023-12-19 08:59:39 +0200

    2023 day17 p2

--- 2023/day19/solution.ts
+++ 2023/day19/solution.ts
@@ -3,96 +3,125 @@ import { Solution, readInput, title, withTime } from "../../common/types";
 import { sum } from "../utils";
 
 
-type Rating = { x: number; m: number; a: number; s: number }
+type Rating = {[key: string]: number}
+
+type Workflow = {rules: Array<{key:string, cmp: string, n: number, target: string}>, fallback: string};
+
+const ops = {
+  '>': (a: number, b: number) => a > b,
+  '<': (a: number, b: number) => a < b
+};
+
+type OpsType = keyof typeof ops;
 
 const part1 = (data: string) => {
   let [workflows, ratingsLines] = data.split('\n\n')
 
   let ratings: Rating[] = ratingsLines.split('\n').map(line => {
     line = line.slice(1, line.length - 1);
-    let rr = line.split(',').map(s => Number(s.split('=')[1]));
-    return {
-      x: rr[0],
-      m: rr[1],
-      a: rr[2],
-      s: rr[3],
+    return Object.fromEntries(line.split(',').map(s => s.split('=')).map(([k, v]) => [k, Number(v)]));
+  });
+
+  let map: Record<string, Workflow> = {};
+  workflows.split('\n').forEach(line => {
+    let [key, rulesS] = line.slice(0, line.length - 1).split('{');
+    let rules = rulesS.split(',');
+    let fallback = rules.pop()!;
+    rules = rules.map(r => r.trim());
+    map[key] = {rules: rules.map(r => {
+      let [comparison, target] = r.split(':')
+      let key = comparison[0];
+      let cmp = comparison[1];
+      let n = Number(comparison.slice(2));
+      return {key, cmp, n, target};
+    }), fallback};
+  });
+
+  let accept = (part: Rating, name = 'in'): boolean => {
+    if (name == 'R') {
+      return false;
     }
-  });
-
-  let map: Record<string, string[]> = {};
-  let acceptedParts: Rating[] = [];
-
-  workflows.split('\n').map(line => {
-    let [key, rules] = line.split('{');
-    rules = rules.slice(0, rules.length - 1);
-    map[key] = rules.split(',')
-  });
-
-  // console.log(map)
-
-
-  for (let part of ratings) {
-
-    let currentWorkflow = map['in'];
-    console.log(currentWorkflow)
-    let workFlowPos = 0;
-    while (true) {
-      let rule = currentWorkflow[workFlowPos];
-      console.log(rule)
-      if (rule == 'A') {
-        acceptedParts.push(part);
-        break;
-      } else if (rule == 'R') {
-        break;
-      } else if (rule.includes('>')) {
-        let [prop, cond] = rule.split('>');
-        let [val, then] = cond.split(':');
-        if (part[prop as keyof Rating] > Number(val)) {
-          if (then == 'A') {
-            acceptedParts.push(part);
-            break;
-          } else if (then == 'R') {
-            break;
-          } else {
-            currentWorkflow = map[then];
-            workFlowPos = 0;
-          }
-        } else {
-          workFlowPos++;
-        }
-      } else if (rule.includes('<')) {
-        let [prop, cond] = rule.split('<');
-        let [val, then] = cond.split(':');
-        if (part[prop as keyof Rating] < Number(val)) {
-          if (then == 'A') {
-            acceptedParts.push(part);
-            break;
-          } else if (then == 'R') {
-            break;
-          } else {
-            currentWorkflow = map[then];
-            workFlowPos = 0;
-          }
-        } else {
-          workFlowPos++;
-        }
-      } else {
-        currentWorkflow = map[rule];
-        console.log(currentWorkflow, rule)
-
-        workFlowPos = 0;
+    if (name == 'A') {
+      return true;
+    }
+    let {rules, fallback} = map[name];
+    for (let {key, cmp, n, target} of rules) {
+      if (ops[cmp as OpsType](part[key], n)) {
+        return accept(part, target);
       }
-
     }
+    return accept(part, fallback);
   }
 
-  // console.log(acceptedParts)
-
-  return acceptedParts.map(p => Object.values(p).reduce(sum)).reduce(sum)
+  return ratings.filter(part => accept(part)).map(p => Object.values(p).reduce(sum)).reduce(sum)
 }
 
 const part2 = (data: string) => {
+  let [workflows, ratingsLines] = data.split('\n\n')
 
+  let map: Record<string, Workflow> = {};
+  workflows.split('\n').forEach(line => {
+    let [key, rulesS] = line.slice(0, line.length - 1).split('{');
+    let rules = rulesS.split(',');
+    let fallback = rules.pop()!;
+    rules = rules.map(r => r.trim());
+    map[key] = {rules: rules.map(r => {
+      let [comparison, target] = r.split(':')
+      let key = comparison[0];
+      let cmp = comparison[1];
+      let n = Number(comparison.slice(2));
+      return {key, cmp, n, target};
+    }), fallback};
+  });
+
+  let count = (ranges: Record<string, number[]>, name = "in"): number => {
+    if (name == 'R') {  
+      return 0;
+    }
+    if (name == 'A') {
+      let product = 1;
+      for (let [low, high] of Object.values(ranges)) {
+        product *= high - low + 1;
+      }
+      return product;
+    }
+    
+    let {rules, fallback} = map[name];
+    let total = 0;
+    let comeToEnd = false;
+    for (let {key, cmp, n, target} of rules) {
+      let [low, high] = ranges[key];
+      let trueHalf: number[];
+      let falseHalf: number[];
+      if (cmp == '<') {
+        trueHalf = [low, n-1];
+        falseHalf = [n, high];
+      } else {
+        trueHalf = [n+1, high];
+        falseHalf = [low, n];
+      }
+
+      let found = false;
+      if (trueHalf[0] <= trueHalf[1]) {
+        found = true;
+        total += count({...ranges, [key]: trueHalf}, target);
+      }
+      if (falseHalf[0] <= falseHalf[1]) {
+        found = true;
+        ranges = {...ranges, [key]: falseHalf};
+      }
+      if (!found) {
+        comeToEnd = true;
+        break;
+      }
+    }
+    if (!comeToEnd) {
+      total += count(ranges, fallback);
+    }
+    return total;
+  };
+
+  return count(Object.fromEntries("xmas".split('').map(key => [key, [1, 4000]])));
 }
 
 export const solve: Solution = (source) => {
