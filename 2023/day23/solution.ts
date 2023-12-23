9fd0ed75b80e97f912e7619e0c73be951b8dcdb0
jaros <judin.jaroslav@gmail.com>
2023-12-23 10:28:18 +0200

    bfs to dfs

--- 2023/day23/solution.ts
+++ 2023/day23/solution.ts
@@ -7,13 +7,17 @@ const dirs: number[][] = [
   [-1, 0]
 ];
 
-type Point = {r: number, c: number};
+type Point = { r: number, c: number };
 
-let encode = (r: number, c: number) => `${r}:${c}`;
+let encode = (p: Point) => `${p.r}:${p.c}`;
+let decode = (p: string): Point => {
+ let [r, c] = p.split(':').map(Number);
+ return {r,c};
+};
 
 type Way = {
   seen: Set<string>;
-  path: Point[]
+  current: Point;
 }
 
 const steepSlopes = ['^', '>', 'v', '<'];
@@ -25,53 +29,128 @@ const part1 = (data: string) => {
   let grid: string[][] = data.split('\n').map(row => row.split(''));
   let len = grid.length;
 
-  let findS = (): Point => ({r: 0, c: grid[0].findIndex(c => c == '.')});
-  let findF = (): Point => ({r: len-1, c: grid[len-1].findIndex(c => c == '.')});
-
-  let start = findS();
-  let finish = findF();
-
+  let start = { r: 0, c: grid[0].findIndex(c => c == '.') };
+  let finish = { r: len - 1, c: grid[len - 1].findIndex(c => c == '.') };
   let isFinish = (point: Point) => point.r == finish.r && point.c == finish.c;
 
-  let canMove = (r: number, c: number, seen: Set<string>) => {
-    let next = grid[r][c];
+  let canMove = (nP: Point, seen: Set<string>) => {
+    let next = grid[nP.r][nP.c];
     if (steepSlopes.includes(next)) {
       let [dr, dc] = mapSlopes[next];
-      return !seen.has(encode(r + dr, c + dc));
+      return !seen.has(encode({r: nP.r + dr, c: nP.c + dc}));
     }
-    return next != '#' 
+    return next != '#'
   }
 
   // starting point is not counted
-  let q: Way[] = [{path: [start], seen: new Set<string>()}];
 
-  let maxPath = 0;
-  while (q.length > 0) { // BFS
-    let {seen, path} = q.shift()!;
-    let current = path[path.length-1];
+  let dfs = (current: Point, path: Set<string>): number => {
     if (isFinish(current)) {
-      maxPath = Math.max(maxPath, seen.size);
-      continue;
+      return path.size;
     }
-
+    let neighborsPaths = [];
     for (let [dr, dc] of dirs) {
-      let nr = current.r + dr;
-      let nc = current.c + dc;
-      let key = encode(nr, nc);
-      if (nr >= 0 && nr < len && nc >= 0 && nc < len && !seen.has(key) && canMove(nr, nc, seen)) {
-        let nPath: Point[] = [...path, {r: nr, c: nc}]
-        let nSeen = new Set(seen);
+      let nP = { r: current.r + dr, c: current.c + dc };
+      // let nr = current.r + dr;
+      // let nc = current.c + dc;
+      let key = encode(nP);
+      if (nP.r >= 0 && nP.r < len && nP.c >= 0 && nP.c < len && !path.has(key) && canMove(nP, path)) {
+        let nSeen = new Set(path);
         nSeen.add(key)
-        q.push({path: nPath, seen: nSeen});
+        neighborsPaths.push(dfs(nP, nSeen));
+      }
+    }
+    return Math.max(...neighborsPaths)
+  }
+  return dfs(start, new Set());
+};
+
+const part2 = (data: string) => {
+  let grid: string[][] = data.split('\n').map(row => row.split(''));
+  let len = grid.length;
+
+  let start = { r: 0, c: grid[0].findIndex(c => c == '.') };
+  let finish = { r: len - 1, c: grid[len - 1].findIndex(c => c == '.') };
+  let isFinish = (point: Point) => point.r == finish.r && point.c == finish.c;
+  let canMove = (r: number, c: number) => grid[r][c] != '#';
+
+  let points: string[] = [encode(start), encode(finish)];
+
+  let tileDirs: Record<string, number[][]> = {
+    '^': [[-1, 0]], 
+    '>': [[0, 1]], 
+    'v': [[1, 0]], 
+    '<': [[0, -1]],
+    '.': dirs
+  }
+
+  for (let r = 0; r < len; r++) {
+    for (let c = 0; c < len; c++) {
+
+      if (grid[r][c] == '#') {
+        continue;
+      }
+      let neighbors = 0;
+      for (let [dr, dc] of dirs) {
+        let nr = r + dr;
+        let nc = c + dc;
+        if (nr >= 0 && nr < len && nc >= 0 && nc < len && canMove(nr, nc)) {
+          neighbors++;
+        }
+      }
+      if (neighbors >= 3) {
+        points.push(encode({ r, c }))
       }
     }
   }
 
-  return maxPath;
-};
+  // console.log(points)
 
-const part2 = (data: string) => {
-  return 0;
+  let graph: Record<string, Record<string, number>> = Object.fromEntries(points.map(pt => [pt, {} as Record<string, number>]));
+
+  for (let pt of points) {
+    let {r: sr, c: sc} = decode(pt);
+    let stack = [[0, sr, sc]]; // first time n=0
+    let edgeKey = encode({r: sr, c: sc});
+    let seen = new Set([edgeKey]);
+
+    while (stack.length > 0) {
+      let [n, r, c] = stack.pop()!
+      if (n != 0 && points.includes(encode({r,c}))) {
+        graph[edgeKey][encode({r,c})] = n
+        continue;
+      }
+      for (let [dr, dc] of tileDirs[grid[r][c]]) {
+        let nr = r + dr
+        let nc = c + dc
+        let newKey = encode({r: nr, c: nc})
+        if (nr >= 0 && nr < len && nc >= 0 && nc < len && canMove(nr, nc) && !seen.has(newKey)) {
+          stack.push([n+1, nr, nc])
+          seen.add(newKey)
+        }
+      }
+    }
+  }
+  // console.log(graph)
+
+  let seen = new Set<string>();
+
+  let dfs = (pt: Point): number => {
+    if (isFinish(pt)) {
+      return 0;
+    }
+
+    let m = Number.NEGATIVE_INFINITY;
+    let ptKey = encode(pt);
+    seen.add(ptKey)
+    for (let nx in graph[ptKey] ) {
+      m = Math.max(m, dfs(decode(nx)) + graph[ptKey][nx])
+    }
+    seen.delete(ptKey)
+    return m;
+  }
+  
+  return dfs(start);
 };
 
 export const solve: Solution = (source) => {
