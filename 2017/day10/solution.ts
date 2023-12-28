2d2b6590a6d74dfa788fbb7efd476be5acf26784
jaros <judin.jaroslav@gmail.com>
2023-12-28 13:32:35 +0200

    2017 day10 p2

--- 2017/day10/solution.ts
+++ 2017/day10/solution.ts
@@ -30,7 +30,41 @@ const part1 = (data: string) => {
 
 
 const part2 = (data: string) => {
-  return 0;
+  let lengths = data.split('').map(ch => ch.charCodeAt(0));
+  const suffix = [17, 31, 73, 47, 23];
+  lengths.push(...suffix)
+
+  let list: number[] = [];
+  for (let i = 0; i < 256; i++) { list.push(i) }
+
+  let mod = list.length;
+
+  let currPos = 0;
+  let skipSize = 0;
+
+  for (let r = 0; r < 64; r++) {
+    for (let length of lengths) {
+      let from = currPos;
+      let to = (from + length - 1);
+      while (from < to) { // swap
+        let tmp = list[from % mod];
+        list[from % mod] = list[to % mod];
+        list[to % mod] = tmp;
+        from++;
+        to--;
+      }
+      currPos = (currPos + length + skipSize) % mod;
+      skipSize++;
+    }
+  }
+
+  let leadingZero = (s: string) => s.length < 2 ? '0' + s : s;
+
+  let denseHash = [];
+  for (let i = 0; i < list.length; i += 16) {
+    denseHash.push(list.slice(i, i + 16).reduce((a, b) => a ^ b).toString(16))
+  }
+  return denseHash.map(leadingZero).join('')
 }
 
 export const solve: Solution = (source) => {
