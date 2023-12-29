90c1040e4063db140e5ec7da6a47ee8ef2afa0bb
jaros <judin.jaroslav@gmail.com>
2023-12-29 20:54:03 +0200

    2017 day13 p2

--- 2017/day13/solution.ts
+++ 2017/day13/solution.ts
@@ -1,36 +1,69 @@
 import { sum } from "../../2023/utils";
 import { Solution, readInput, title, withTime } from "../../common/types";
 
-const part1 = (data: string) => {
-  let layers: Array<{ range: number, pos: number, inc: 1 | -1 }> = [];
-  let scannerLayers: number[] = [];
+type Layer = { range: number, pos: number, inc: 1 | -1 };
 
+let initLayers: Layer[] = [];
+let scannerLayers: number[] = [];
+
+let simulateScanner = (layers: Layer[]) => {
+  for (let sc of scannerLayers) {
+    let l = layers[sc];
+    if (l.inc == 1 && l.pos == l.range) {
+      l.inc = -1;
+    } else if (l.inc == -1 && l.pos == 1) {
+      l.inc = 1;
+    }
+    l.pos += l.inc;
+  }
+}
+
+let isCaught = (layers: Layer[]) => {
+  for (let packIdx = 0; packIdx < layers.length; packIdx++) {
+    if (layers[packIdx]?.pos == 1) {
+      return true;
+    }
+    simulateScanner(layers);
+  }
+  return false;
+}
+
+let init = (data: string) => {
+  initLayers = [];
+  scannerLayers = [];
   for (let row of data.split('\n')) {
     let [depth, range] = row.split(': ').map(Number)
-    layers[depth] = { range, pos: 1, inc: 1 };
+    initLayers[depth] = { range, pos: 1, inc: 1 };
     scannerLayers.push(depth);
   }
+}
+
+const part1 = (data: string) => {
+  init(data);
+  let layers = initLayers;
   let total = 0;
 
   for (let packIdx = 0; packIdx < layers.length; packIdx++) {
     if (layers[packIdx]?.pos == 1) {
       total += packIdx * layers[packIdx].range;
     }
-    for (let sc of scannerLayers) {
-      let l = layers[sc];
-      if (l.inc == 1 && l.pos == l.range) {
-        l.inc = -1;
-      } else if (l.inc == -1 && l.pos == 1) {
-        l.inc = 1;
-      }
-      l.pos += l.inc;
-    }
+    simulateScanner(layers);
   }
   return total;
 };
 
 const part2 = (data: string) => {
-  return 0;
+  init(data);
+  let skip = 0;
+  let ll: Layer[] = initLayers;
+  while (true) {
+    simulateScanner(ll);
+    skip++;
+    if (!isCaught(initLayers.map(l => ({ ...l })))) { // if (!isCaught(JSON.parse(JSON.stringify(initLayers)))) { // 41.74 vs 8.66 => 80% diff
+      break;
+    }
+  }
+  return skip;
 };
 
 export const solve: Solution = (source) => {
