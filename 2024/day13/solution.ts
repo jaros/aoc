6b220e7789dec926ad0c16e2bde6bb3e972dc3bc
Jaroslav Judin <jaroslav.judin@bolt.eu>
2024-12-13 15:49:06 +0200

    2024 d13

--- 2024/day13/solution.ts
+++ 2024/day13/solution.ts
@@ -1,13 +1,37 @@
 import { sum } from "../../2023/utils";
 import { Solution, readInput, title, withTime } from "../../common/types";
 
-const parseInput = (data: string) => {
-  let lines = data.split("\n");
-  return lines;
+const nums = (s: string): number[] => {
+  return s.match(/\d+/g)!.map(Number)
 };
 
 const part1 = (data: string) => {
-  return -1;
+  // let lines = parseInput(data);
+  // console.log(lines)
+  let ans = 0
+  for (let l of data.split("\n\n")) {
+    let ll = l.split("\n")
+    let a = nums(ll[0])
+    let b = nums(ll[1])
+    let p = nums(ll[2])
+    // console.log(a)
+    let best = 5000
+
+    for (let i=0; i < 101; i++) {
+      for (let j=0; j<101; j++) {
+        let nx = a[0]*i + b[0]*j
+        let ny = a[1]*i + b[1]*j
+        if (p[0] == nx && p[1] == ny) {
+          best = Math.min(best, 3*i+j)
+        }
+      }
+    }
+    if (best < 5000) {
+      ans += best
+    }
+ 
+  }
+  return ans;
 };
 
 const part2 = (data: string) => {
@@ -15,7 +39,7 @@ const part2 = (data: string) => {
 };
 
 export const solve: Solution = (source) => {
-  title("Day 13: ");
+  title("Day 13: Claw Contraption");
   const data = readInput(source, import.meta.dir);
   withTime(part1)(data);
   withTime(part2)(data);
