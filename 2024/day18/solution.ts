ea77ce5a5c337e15893eceb935043fed5ac197e4
Jaroslav Judin <jaroslav.judin@bolt.eu>
2024-12-18 22:58:40 +0200

    2024 d18 p2

--- 2024/day18/solution.ts
+++ 2024/day18/solution.ts
@@ -11,8 +11,15 @@ type Position = [number, number];
 let printGrid = (grid: string[][]) => {
   console.log(grid.map((r) => r.join("")).join("\n"));
 };
-const part1 = (data: string) => {
-  let bytes = data.split("\n").map((line) => line.split(",").map(Number));
+
+const dirs: Position[] = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1],
+];
+
+const initGrid = (coords: number[][], limit: number): string[][] => {
   let grid: string[][] = [];
   for (let y = 0; y < SIZE; y++) {
     let row = [];
@@ -21,19 +28,18 @@ const part1 = (data: string) => {
     }
     grid.push(row);
   }
-  // printGrid(grid);
 
-  for (let i = 0; i < LIMIT; i++) {
-    let [x, y] = bytes[i];
+  for (let i = 0; i < limit; i++) {
+    let [x, y] = coords[i];
     grid[y][x] = "#";
   }
+  return grid;
+};
 
-  const dirs: Position[] = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-  ];
+const part1 = (data: string) => {
+  const coords = data.split("\n").map((line) => line.split(",").map(Number));
+
+  const grid = initGrid(coords, LIMIT);
 
   let stack: number[][] = [[0, 0, 0]]; // r, c, d
   let seen = new Set([toKey(0, 0)]);
@@ -62,8 +68,50 @@ const part1 = (data: string) => {
 };
 
 const part2 = (data: string) => {
-  let ans = 0;
-  return ans;
+  const coords = data.split("\n").map((line) => line.split(",").map(Number));
+
+  const hasPath = (n: number): boolean => {
+    const grid = initGrid(coords, n);
+
+    let stack: number[][] = [[0, 0, 0]]; // r, c, d
+    let seen = new Set([toKey(0, 0)]);
+    while (stack.length > 0) {
+      let [r, c, d] = stack.shift()!;
+      for (let [dr, dc] of dirs) {
+        let nr = r + dr;
+        let nc = c + dc;
+
+        if (
+          nr >= 0 &&
+          nr < SIZE &&
+          nc >= 0 &&
+          nc < SIZE &&
+          grid[nr][nc] != "#" &&
+          !seen.has(toKey(nr, nc))
+        ) {
+          if (nr == SIZE - 1 && nc == SIZE - 1) {
+            return true;
+          }
+          seen.add(toKey(nr, nc));
+          stack.push([nr, nc, d + 1]);
+        }
+      }
+    }
+    return false;
+  };
+
+  let l = 0;
+  let r = coords.length - 1;
+
+  while (l < r) {
+    let mid = Math.floor(l + r) / 2;
+    if (hasPath(mid + 1)) {
+      l = mid + 1;
+    } else {
+      r = mid;
+    }
+  }
+  return coords[l];
 };
 
 export const solve: Solution = (source) => {
