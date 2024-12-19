68f2e0d2256b72bb873184a51c951a0dda832edd
Jaroslav Judin <jaroslav.judin@bolt.eu>
2024-12-19 14:20:22 +0200

    2024 d19 refactor - extract common func

--- 2024/day19/solution.ts
+++ 2024/day19/solution.ts
@@ -1,43 +1,8 @@
 import { sum } from "../../2023/utils";
 import { Solution, readInput, title, withTime } from "../../common/types";
 
-const part1 = (data: string) => {
-  let [patt, dess] = data.split("\n\n");
-  let patterns = patt.split(", ");
-  let designs = dess.split("\n")
 
-  const maxLen = Math.max(...patterns.map(p => p.length));
-  
-  const pp = new Set(patterns);
-
-  let cache: Map<string, boolean> = new Map();
-
-  const isPossible = (design: string): boolean => {
-    if (cache.has(design)) {
-      return cache.get(design)!;
-    }
-    if (design.length == 0) {
-      return true;
-    }
-    let possibilities = []
-    for (let i=1; i <= maxLen; i++) {
-      if (design.length >= i) {
-        let prefix = design.slice(0, i);
-        if (pp.has(prefix)) {
-          let key = design.slice(i);
-          let branch = isPossible(design.slice(i))
-          cache.set(key, branch)
-          possibilities.push(branch)
-        }
-      }
-    }
-    return possibilities.some(val => val);
-  }
-
-  return designs.filter(isPossible).length;
-};
-
-const part2 = (data: string) => {
+const buildWaysFinder = (data: string) => {
   let [patt, dess] = data.split("\n\n");
   let patterns = patt.split(", ");
   let designs = dess.split("\n")
@@ -69,7 +34,16 @@ const part2 = (data: string) => {
     }
     return possibilities.reduce(sum, 0);
   }
+  return {possibleWays, designs}
+}
 
+const part1 = (data: string) => {
+  const {designs, possibleWays} = buildWaysFinder(data);
+  return designs.map(possibleWays).filter(waysCount => waysCount > 0).length;
+};
+
+const part2 = (data: string) => {
+  const {designs, possibleWays} = buildWaysFinder(data);
   return designs.flatMap(possibleWays).reduce(sum, 0);
 };
 
