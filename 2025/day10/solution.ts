7929972d0695b08bec1f86eda6a135551a4668d0
Jaroslav Judin <jaroslav.judin@bolt.eu>
2025-12-10 15:47:45 +0200

    day10 p2: add working test case

--- 2025/day10/solution.ts
+++ 2025/day10/solution.ts
@@ -1,22 +1,26 @@
 
+import { i } from "mathjs";
 import { sum } from "../../2023/utils";
 import { Solution, readInput, title, withTime } from "../../common/types";
 
 type Schema = string;
 type Button = number[];
-type Device = {schema: Schema, buttons: Button[]};
+type Joltage = number[];
+type Device = {schema: Schema, buttons: Button[], joltage: Joltage};
 
 const getLines = (data: string) => data.split("\n")
 
 const getDevices = (lines: string[]): Device[] => {
   return lines.map(line => {
-    const [schemaStr, ...buttonsStr] = line.split(" ");
+    const [schemaStr, ...rest] = line.split(" ");
     const schema = schemaStr.substring(1, schemaStr.length-1) as Schema;
-    buttonsStr.splice(buttonsStr.length-1, 1);
-    const buttons = buttonsStr.map(str => {
+    const restNums = rest.map(str => {
       return str.substring(1, str.length-1).split(",").map(numStr => Number(numStr)) as Button;
     });
-    return {schema, buttons}
+    const buttons = restNums.slice(0, rest.length-1);
+    const joltage = restNums.at(-1) as Joltage;
+    // console.log({schema, buttons, joltage})
+    return {schema, buttons, joltage}
   })
 }
 
@@ -32,14 +36,26 @@ function getAllCombinations<T>(arr: T[]): T[][] {
   const result: T[][] = [[]]; // start with empty set
 
   for (const item of arr) {
-    const newCombos = result.map(combo => [...combo, item]);
-    result.push(...newCombos);
+    const newCombos: T[][] = [];
+    for (let combo of result) {
+      let nRes = []
+      for (let c of combo) {
+        nRes.push(c);
+      }
+      nRes.push(item);
+      newCombos.push(nRes)
+    }
+    // const newCombos = result.map(combo => [...combo, item]);
+    for (let c of newCombos) {
+      result.push(c)
+    }
+    // result.push(...newCombos);
   }
 
   return result;
 }
 
-const countMinPresses = (device: Device): number => {
+const countMinPressesForSchemaMatch = (device: Device): number => {
   const finalState = schemaState(device.schema);
   const equalToSchema = (other: boolean[]): boolean => {
     for (let i = 0; i < finalState.length; i++) {
@@ -80,12 +96,78 @@ const countMinPresses = (device: Device): number => {
 
 const part1 = (data: string) => {
   const devices = getDevices(getLines(data));
-  return devices.map(d => countMinPresses(d)).reduce(sum);
+  return devices.map(d => countMinPressesForSchemaMatch(d)).reduce(sum);
+}
+
+const countMinPressesForJoltageMatch = (device: Device): number => {
+  const press = (counters: number[], btn: number[]) => {
+    for (let idx of btn) {
+      counters[idx]++;
+    }
+  }
+
+  const compareToJoltage = (counters: number[]): "same" | "less" | "over" => {
+    let same = 0;
+    for (let i = 0; i < device.joltage.length; i++) {
+      if (counters[i] > device.joltage[i]) {
+        return "over";
+      } else if (counters[i] == device.joltage[i]) {
+        same++;
+      }
+    }
+    return same == counters.length ? "same" : "less";
+  };
+
+  let minTimes = Number.MAX_SAFE_INTEGER;
+  
+  const visited: Set<string> = new Set();
+  const toComboRecord = (combo: number[]) => combo.join();
+
+  // queue for BFS
+  const q: Array<{pressed: number[], count: number, joltage: Joltage}> = [];
+  // first time init with single button press
+  for (let i=0; i < device.buttons.length; i++) {
+    let btn = device.buttons[i];
+    const joltage =  Array(device.joltage.length).fill(0);
+    press(joltage, btn);
+    const combo = Array(device.buttons.length).fill(0)
+    combo[i] = 1
+    q.push({pressed: combo, count: 1, joltage})
+  }
+
+  
+  while (q.length > 0) {
+    const {pressed, count, joltage} = q.shift()!;
+    if (count >= minTimes) {
+      continue;
+    }
+    visited.add(toComboRecord(pressed));
+    const res = compareToJoltage(joltage);
+    if (res == "same") {
+      minTimes = Math.min(minTimes, count);
+    } else if (res == "less") {
+      for (let i=0; i < device.buttons.length; i++) {
+        let btn = device.buttons[i];
+        let newPressed = [...pressed];
+        newPressed[i]++;
+        if (visited.has(toComboRecord(newPressed))) {
+          continue;
+        }
+        let newJoltage = [...joltage];
+        press(newJoltage, btn);
+        q.push({ pressed: newPressed, count: count+1, joltage: newJoltage });
+      }
+    } else {
+        // over, do nothing
+    }
+  }
+
+  return minTimes;
 }
 
 const part2 = (data: string) => {
-  const lines = getLines(data);
-  return 0;
+  const devices = getDevices(getLines(data));
+  return devices.map(d => countMinPressesForJoltageMatch(d)).reduce(sum);
 }
 
 export const solve: Solution = (source) => {
